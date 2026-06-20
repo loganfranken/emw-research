bf6f84f4fb859fdf8c2636280cdfe40d67efbf6c
Logan Franken <loganfranken@gmail.com>
2026-06-20 02:40:35 -0400

    Updated cohort display logic

--- script/main.js
+++ script/main.js
@@ -1,5 +1,7 @@
 const MINIMUM_TYPING_TIMEOUT = 40;
 const MAXIMUM_TYPING_TIMEOUT = 50;
+const HIDDEN_CSS_CLASS = "hidden";
+const COHORT_HEADERS = [...document.querySelectorAll(".cohort-header")];
 
 const getRandomTypingTimeout = () => Math.floor(Math.random() * (MAXIMUM_TYPING_TIMEOUT - MINIMUM_TYPING_TIMEOUT) + MAXIMUM_TYPING_TIMEOUT);
 
@@ -28,8 +30,37 @@ const typingOps = [...document.querySelectorAll("[data-typing]")].map((elem) => 
     });
 });
 
+const timeout = (duration) => {
+    return new Promise((resolve) => {
+        setTimeout(() => { resolve(); }, duration)
+    });
+};
+
+COHORT_HEADERS.forEach((elem) => {
+    elem.classList.add("pre-fade-in");
+});
+
+[...document.querySelectorAll("[data-toggles]")].forEach((elem) => {
+    const listId = elem.getAttribute("data-toggles");
+    const list = document.getElementById(listId);
+
+    list.classList.add(HIDDEN_CSS_CLASS);
+    elem.addEventListener("click", () => {
+        if(list.classList.contains(HIDDEN_CSS_CLASS)) {
+            list.classList.remove(HIDDEN_CSS_CLASS);
+        } else {
+            list.classList.add(HIDDEN_CSS_CLASS);
+        }
+    });
+});
+
 (async () => {
     for(var i = 0; i < typingOps.length; i++) {
         await typingOps[i]();
+    }
+
+    for(var i = 0; i < COHORT_HEADERS.length; i++) {
+        await timeout(50);
+        COHORT_HEADERS[i].classList.add("fade-in");
     }
 })();
